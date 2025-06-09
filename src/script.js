5c0c249be7920a9047f9ae616916757c8c699867
Zachary <z.dsd2023@gmail.com>
2025-06-08 17:05:56 -0700

    stopwatch function implemented

---
+++ src/script.js
@@ -0,0 +1,49 @@
+const startBtn = document.getElementById("start");
+const endBtn = document.getElementById("stop");
+const restartBtn = document.getElementById("reset");
+
+
+let minute = 0;
+let second = 0;
+let count = 0;
+startBtn.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+    console.log(second);
+})
+endBtn.addEventListener("click", function () {
+    timer = false;
+})
+restartBtn.addEventListener("click", function (){
+    timer = false;
+    minute = 0;
+    second = 0;
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+
+})
+function stopWatch() {
+    if(timer){
+        count++
+        console.log(count)
+        if(count==100){
+            second++;
+            count = 0;
+        }
+        if(second==60){
+            minute++;
+            second = 0;
+        }
+        let minString = minute
+        let secondString = second
+        if(minute<10){
+            minString = "0" + minute;
+        }
+        if(second<10){
+            secondString = "0" + second;
+        }
+        document.getElementById("minutes").innerHTML = minString;
+        document.getElementById("seconds").innerHTML = secondString;
+        setTimeout(stopWatch, 10);
+    }
+}
